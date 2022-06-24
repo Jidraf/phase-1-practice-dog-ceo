2ac27750dd543e83590560d0a515288350e3b80d
Jidraf Kimachia <raphjid170@gmail.com>
2022-06-25 00:31:18 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", function(){
+    fetchDogImages()
+    
+})
+
+
+function fetchDogImages(){
+    fetch("https://dog.ceo/api/breeds/image/random/4")
+    .then(response =>  {return response.json()})
+    .then(data => {
+        let images = data.message
+        images.forEach(image => loadImagestoDOM(image))
+        })
+}
+
+
+function loadImagestoDOM(image){
+    let dogsCont = document.getElementById("dog-image-container")
+
+    let ourImage = document.createElement('img')
+
+    ourImage.src = image
+
+    dogsCont.appendChild(ourImage)
+}
